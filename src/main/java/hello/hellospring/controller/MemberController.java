c2a4665d691293154bfd6ab91406a6dbcec0e5ef
최재현 <apple@jays-mac.local>
2022-07-13 00:00:38 +0900

    회원 웹 기능 조회

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,9 +1,15 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
+import hello.hellospring.dto.MemberForm;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -19,5 +25,23 @@ public class MemberController {
     @GetMapping("/members/new")
     public String createForm() {
         return "members/createMemberForm";
+    }
+
+    @PostMapping
+    public String create(MemberForm form)
+    {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String List(Model model)
+    {
+        List<Member> memberList = memberService.findMembers();
+        model.addAttribute("members",memberList);
+        return "/members/memberList";
     }
 }
