e4d22e7342c99cfc13fae9ba400cffd7c56474d9
최재현 <apple@jays-mac.local>
2022-07-12 23:45:30 +0900

    회원 웹 기능 홈화면 추가 및 등록

---
+++ src/main/java/hello/hellospring/config/SpringConfig.java
@@ -0,0 +1,23 @@
+package hello.hellospring.config;
+
+import com.sun.source.tree.MemberReferenceTree;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// Bean 어노테이션으로 컴포넌트 스캔을 만들 수 있다.
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
