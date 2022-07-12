acae6f62de7936b7e147a5737ddc6fd1b82d8a78
최재현 <apple@jays-mac.local>
2022-07-13 02:33:53 +0900

    jdbc, jdbcTemplate 사용해서 리포지토리 구현

--- src/main/java/hello/hellospring/config/SpringConfig.java
+++ src/main/java/hello/hellospring/config/SpringConfig.java
@@ -1,15 +1,27 @@
 package hello.hellospring.config;
 
 import com.sun.source.tree.MemberReferenceTree;
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // Bean 어노테이션으로 컴포넌트 스캔을 만들 수 있다.
 @Configuration
 public class SpringConfig {
+
+    DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -18,6 +30,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
